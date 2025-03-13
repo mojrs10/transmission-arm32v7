cf15adb62809c57610a4c1d535490b5e8b0807ac
mojrs10 <64679555+mojrs10@users.noreply.github.com>
2025-03-13 20:28:51 +0100

    Update fix-permissions.sh

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -12,11 +12,13 @@ chmod 755 root/app/blocklist-update.sh
 chmod 755 root/etc/s6-overlay/s6-rc.d/init-transmission-config/run
 chmod 755 root/etc/s6-overlay/s6-rc.d/svc-transmission/run
 chmod 755 root/etc/s6-overlay/s6-rc.d/svc-transmission/finish
+chmod 755 root/etc/s6-overlay/s6-rc.d/init-crontabs-config/run
 
 git add root/app/blocklist-update.sh
 git add root/etc/s6-overlay/s6-rc.d/init-transmission-config/run
 git add root/etc/s6-overlay/s6-rc.d/svc-transmission/run
 git add root/etc/s6-overlay/s6-rc.d/svc-transmission/finish
+git add root/etc/s6-overlay/s6-rc.d/init-crontabs-config/run
 
 # Provjeri je li bilo promjena
 if ! git diff-index --quiet HEAD --; then
